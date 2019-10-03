b031342842351ab1d7a0198042e4751cdd0492db
Developer <developer@ouhk.edu.hk>
2019-10-03 12:41:23 +0800

    lab04

--- interests.js
+++ interests.js
@@ -1,5 +1,6 @@
 const http = require('http'); 
 const url = require('url');
+
 /*
 function SimpleInterests() {
 	this.principal = p;
@@ -11,6 +12,7 @@ function SimpleInterests() {
 const principal = 1000000;
 const rate = 0.01;
 const period = 10;
+const numberCompound = 12;
 
 class SimpleInterests {
 	constructor(p = principal,r = rate,t = period) { // default parameter values
@@ -25,6 +27,20 @@ class SimpleInterests {
 ${this.rate *100}% for ${this.period} years = $${this.interests}`);
 	}
 }
+class CompoundInterests {
+	constructor(p = principal,r = rate,t = period, n = numberCompound) { // default parameter values
+		this.principal = p;
+		this.rate = r;
+		this.period = t;
+		this.numberCompound = n;
+		let amount = Math.pow( (1 + ( r / n )), n * t ) * p;
+		this.interests = amount - p;
+	}
+
+	toString() {
+		return(`Interests for $${this.principal} at an annual rate which is compounded per ${this.numberCompound} times of ${this.rate *100}% for ${this.period} years = $${this.interests}`); 
+	}
+}
 
 // function handle_incoming_request(req,res) {
 const handle_incoming_request = (req, res) => {
@@ -32,10 +48,15 @@ const handle_incoming_request = (req, res) => {
     console.log(`Incoming request ${req.method}, ${req.url} received at ${timestamp}`);
 
 	var parsedURL = url.parse(req.url,true); // true to get query as object 
-
-	// extract query string parameters
+	
+	if(parsedURL.query.isCompounded == "true"){
+		var obj = new CompoundInterests(parsedURL.query.p, parsedURL.query.r,parsedURL.query.t, parsedURL.query.n); 
+	
+	}else{
+		// extract query string parameters
 	var obj = new SimpleInterests(parsedURL.query.p, parsedURL.query.r,parsedURL.query.t); 
-
+	}
+	
 	if (parsedURL.query.format == 'json') {
 		res.writeHead(200, {"Content-Type" : "text/json"}); 
 		res.end(JSON.stringify(obj));
